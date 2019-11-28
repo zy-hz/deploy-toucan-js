3ca35f7b93839ce80b2cc79fd8fa3430611cb0c3
zy <yzhang@tcomchina.com>
2019-11-28 13:55:43 +0800

    1.0.8

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       autorestart: true,
       restart_delay: 5000,
       watch: ['upgrade'],
-      watch_delay: 3000,
+      watch_delay: 1000 * 17,
       max_memory_restart: '1G', //最大内存限制数,超出自动重启
       exec_mode: "cluster", //可选值fork,cluster(开启集群模式实现负载均衡),默认fork
       // 设置输出目录后，pm2-logrotate就不生效了（未确认）
